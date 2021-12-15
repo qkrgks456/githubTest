96f2c23c60e485fb992a7e7849616071dfdc1b67
parkhan <6987410@naver.com>
2021-12-15 15:46:08 +0900

    gogo 브랜치 커밋

--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,7 @@ public class Test {
     public static void main(String[] args) {
         // 주석
         // cherry pick test용
+        // 소스트리에서 gogo 브랜치 커밋용
         System.out.println("hello");
     }
 }
